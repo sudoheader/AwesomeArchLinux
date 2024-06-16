731fc382dbc4a4134b6296ded3f2627744d3c8ab
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2024-06-15 20:56:54 -0700

    attempt fix: Increase root partition size and improve script reliability

--- base/archinstall.sh
+++ base/archinstall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-#Description    : Fully encrypted LVM2 on LUKS with UEFI Arch installation script.
-#Author         : @brulliant
-#Linkedin       : https://www.linkedin.com/in/schmidbruno/
+# Enable debug mode
+set -x
+
+# Description    : Fully encrypted LVM2 on LUKS with UEFI Arch installation script.
+# Author         : @brulliant
+# Linkedin       : https://www.linkedin.com/in/schmidbruno/
 
 # Set up the color variables
 BBlue='\033[1;34m'
@@ -39,19 +42,18 @@ echo -e "\n"
 echo -e "${BBlue}Set / and Swap partition size:\n${NC}"
 
 read -p 'Enter the size of SWAP in GB: ' SIZE_OF_SWAP
-read -p 'Enter the size of / in GB, the remaining space will be allocated to /home: ' SIZE_OF_ROOT
+read -p 'Enter the size of / in GB (ensure this is large enough, e.g., at least 20GB): ' SIZE_OF_ROOT
 echo -e "\n"
 
 # Use the correct variable name for the target disk
 DISK="/dev/$TARGET_DISK"
 USERNAME="$NEW_USER"
 HOSTNAME="$NEW_HOST"
-SWAP_SIZE="${SIZE_OF_SWAP}G" # Modern systems don't really need swap.
+SWAP_SIZE="${SIZE_OF_SWAP}G"
 ROOT_SIZE="${SIZE_OF_ROOT}G"
-CRYPT_NAME='crypt_lvm' # the name of the LUKS container.
-LVM_NAME='lvm_arch' # the name of the logical volume.
-LUKS_KEYS='/etc/luksKeys' # Where you will store the root partition key
-
+CRYPT_NAME='crypt_lvm'
+LVM_NAME='lvm_arch'
+LUKS_KEYS='/etc/luksKeys'
 
 # Setting time correctly before installation
 timedatectl set-ntp true
@@ -74,24 +76,21 @@ sgdisk -n 3:1130496:$(sgdisk -E $DISK) -t 3:8309 -c 3:"Linux LUKS" $DISK
 
 # Create the LUKS container
 echo -e "${BBlue}Creating the LUKS container...${NC}"
-#read -ps 'Enter a password for the LUKS container: ' LUKS_PASS
-
-# Encrypts with the best key size.
-cryptsetup -q --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 3000 --use-random  luksFormat --type luks1 $DISK"p3" &&\
+cryptsetup -q --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 3000 --use-random luksFormat --type luks1 $DISK"3" &&\
 
 # Opening LUKS container to test
 echo -e "${BBlue}Opening the LUKS container to test password...${NC}"
-cryptsetup -v luksOpen $DISK"p3" $CRYPT_NAME &&\
+cryptsetup -v luksOpen $DISK"3" $CRYPT_NAME &&\
 cryptsetup -v luksClose $CRYPT_NAME
 
-# create a LUKS key of size 2048 and save it as boot.key
+# Create a LUKS key of size 2048 and save it as boot.key
 echo -e "${BBlue}Creating the LUKS key for $CRYPT_NAME...${NC}"
 dd if=/dev/urandom of=./boot.key bs=2048 count=1 &&\
-cryptsetup -v luksAddKey -i 1 $DISK"p3" ./boot.key &&\
+cryptsetup -v luksAddKey -i 1 $DISK"3" ./boot.key &&\
 
-# unlock LUKS container with the boot.key file
+# Unlock LUKS container with the boot.key file
 echo -e "${BBlue}Testing the LUKS keys for $CRYPT_NAME...${NC}"
-cryptsetup -v luksOpen $DISK"p3" $CRYPT_NAME --key-file ./boot.key &&\
+cryptsetup -v luksOpen $DISK"3" $CRYPT_NAME --key-file ./boot.key &&\
 echo -e "\n"
 
 # Create the LVM physical volume, volume group and logical volume
@@ -102,8 +101,8 @@ lvcreate --verbose -L $ROOT_SIZE $LVM_NAME -n root &&\
 lvcreate --verbose -L $SWAP_SIZE $LVM_NAME -n swap &&\
 lvcreate --verbose -l 100%FREE $LVM_NAME -n home &&\
 
-# Format the partitions 
-echo -e "${BBlue}Formating filesystems...${NC}"
+# Format the partitions
+echo -e "${BBlue}Formatting filesystems...${NC}"
 mkfs.ext4 /dev/mapper/$LVM_NAME-root &&\
 mkfs.ext4 /dev/mapper/$LVM_NAME-home &&\
 mkswap /dev/mapper/$LVM_NAME-swap &&\
@@ -116,44 +115,44 @@ mkdir --verbose /mnt/home &&\
 mount --verbose /dev/mapper/$LVM_NAME-home /mnt/home &&\
 mkdir --verbose -p /mnt/tmp &&\
 
-# Mount efi
+# Mount EFI
 echo -e "${BBlue}Preparing the EFI partition...${NC}"
-mkfs.vfat -F32 $DISK"p2"
+mkfs.vfat -F32 $DISK"2"
 sleep 2
 mkdir --verbose /mnt/efi
 sleep 1
-mount --verbose $DISK"p2" /mnt/efi
+mount --verbose $DISK"2" /mnt/efi
 
 # Create directory and copy the key
-echo -e "${BBlue}Copying the $CRYPT_NAME key to $LUKS_KEYS ...${NC}" 
+echo -e "${BBlue}Copying the $CRYPT_NAME key to $LUKS_KEYS ...${NC}"
 mkdir --verbose /mnt$LUKS_KEYS
 cp ./boot.key /mnt$LUKS_KEYS/boot.key
 
 # Update the keyring for the packages
-echo -e "${BBlue}Updating Arch Keyrings...${NC}" 
+echo -e "${BBlue}Updating Arch Keyrings...${NC}"
 pacman -Sy archlinux-keyring --noconfirm
 
 # Install Arch Linux base system. Add or remove packages as you wish.
-echo -e "${BBlue}Installing Arch Linux base system...${NC}" 
+echo -e "${BBlue}Installing Arch Linux base system...${NC}"
 echo -ne "\n\n\n" | pacstrap -i /mnt base base-devel archlinux-keyring linux linux-headers \
                     linux-firmware zsh lvm2 mtools networkmanager iwd dhcpcd wget curl git \
                     openssh neovim unzip unrar p7zip zip unarj arj cabextract xz pbzip2 pixz \
                     alsa-firmware alsa-tools alsa-utils fuse3 ntfs-3g zsh-completions net-tools sbctl \
                     lrzip cpio gdisk go rust nasm rsync vim nano dosfstools nano-syntax-highlighting usbutils
 
-# Generate fstab file 
-echo -e "${BBlue}Generating fstab file...${NC}" 
+# Generate fstab file
+echo -e "${BBlue}Generating fstab file...${NC}"
 genfstab -pU /mnt >> /mnt/etc/fstab &&\
 
 # Securely delete the key file from the local file system.
-echo -e "${BBlue}Securely erasing the local key file...${NC}" 
+echo -e "${BBlue}Securely erasing the local key file...${NC}"
 shred -u ./boot.key
 
 # Add an entry to fstab so the new mountpoint will be mounted on boot
-echo -e "${BBlue}Adding tmpfs to fstab...${NC}" 
+echo -e "${BBlue}Adding tmpfs to fstab...${NC}"
 echo "tmpfs /tmp tmpfs rw,nosuid,nodev,noexec,relatime,size=2G 0 0" >> /mnt/etc/fstab &&\
 
-echo -e "${BBlue}Adding proc to fstab and harndening it...${NC}" 
+echo -e "${BBlue}Adding proc to fstab and hardening it...${NC}"
 echo "proc /proc proc nosuid,nodev,noexec,hidepid=2,gid=proc 0 0" >> /mnt/etc/fstab &&\
 mkdir /mnt/etc/systemd/system/systemd-logind.service.d &&\
 touch /mnt/etc/systemd/system/systemd-logind.service.d/hidepid.conf &&\
@@ -172,6 +171,6 @@ cp ./chroot.sh /mnt &&\
 chmod +x /mnt/chroot.sh &&\
 shred -u ./chroot.sh
 
-# Chroot into new system and configure it 
+# Chroot into new system and configure it
 echo -e "${BBlue}Chrooting into new system and configuring it...${NC}"
-arch-chroot /mnt /bin/bash ./chroot.sh
+arch-chroot /mnt /bin/bash /mnt/chroot.sh
